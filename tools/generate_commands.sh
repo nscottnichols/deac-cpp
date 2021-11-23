2f82fc6124b9b2c66ae4d27512220adcfe57f4cd
Nathan S. Nichols <8723338+nscottnichols@users.noreply.github.com>
2021-11-23 12:46:14 -0500

    Update generate_commands.sh

--- tools/generate_commands.sh
+++ tools/generate_commands.sh
@@ -1,10 +1,32 @@
-#! /bin/bash
-deac_commands_file="deac_commands";
-save_directory="./deacresults_M512_P16_0.005"
-mkdir -p ${save_directory}
-for i in {0..9999}
+#!/bin/bash
+number_of_reconstructions=1000; #set number of reconstructions
+if [ $# -lt 3 ]; then
+    echo "generate_commands.sh deac_executable isf_file deac_results_directory <deac_commands_file>";
+    exit 1
+fi
+
+DEAC_EXE=$1; #path to DEAC executable
+ISF_FILE=$2; #path to input binary containing imaginary time, intermediate scattering function, and error
+DEAC_RESULTS_DIR=$3; #path to directory to store DEAC results
+if [ $# -lt 4 ]; then
+    DEAC_COMMAND_FILE="deac_commands";
+else
+    DEAC_COMMAND_FILE=$4;#path to directory to DEAC commands file
+fi
+
+rm -f ${DEAC_COMMAND_FILE}
+for seed in $(seq 1 ${number_of_reconstructions})
 do
-    printf -v i_fmt "%06d" $i
-    cmd="{ time ./deac.e -P 16 -M 512 --save_directory ${save_directory} --omega_max 64.0 --normalize --temperature 1.2 --number_of_generations 1000000 --stop_minimum_fitness 0.005 --seed ${i} ./isf_data.dat ; } > ${save_directory}/deac_time_${i_fmt}.dat 2>&1"
-    echo "$cmd" >> ${deac_commands_file} ;
+    cmd="${DEAC_EXE} \
+    -N 1600000 \
+    -T 1.35 \
+    -P 8 \
+    -M 4096 \
+    --normalize \
+    --omega_max 512.0 \
+    --stop_minimum_fitness 1.0 \
+    --save_directory ${DEAC_RESULTS_DIR} \
+    --seed ${seed} \
+    ${ISF_FILE}";
+    echo "${cmd}" >> ${DEAC_COMMAND_FILE};
 done
